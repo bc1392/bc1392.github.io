2fc19c9bba1c06180184560b8fd471e168cd9f45
Ben S <bencraft1392@gmail.com>
2019-11-04 17:45:51 -0500

    Improved animations.

--- script.js
+++ script.js
@@ -12,10 +12,8 @@ $(document).ready(() => {
         $('#fun-spinny-boi').removeClass('do-thing')
     })
 
-    $('.animated').on('animationend', (e) => {
-        if ($(e.target).hasClass('animated')) {
-            $(e.target).removeClass('animated fadeIn fadeInUp fadeInDown slow flip delay-0p5s delay-1s delay-1p5s delay-2s delay-2p5s delay-3s delay-3p5s delay-4s delay-4p5s delay-5s')
-        }
-    })
+    setTimeout(() => {
+        $('.animated').removeClass('animated fadeIn fadeInUp fadeInDown slow flip delay-0p5s delay-1s delay-1p5s delay-2s delay-2p5s delay-3s delay-3p5s delay-4s delay-4p5s delay-5s')
+    }, 3250)
 
 })

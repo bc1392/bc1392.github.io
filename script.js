16b1c3c86cdc41cf176638e3e71fc2620802b641
Ben S <bencraft1392@gmail.com>
2019-11-04 17:42:41 -0500

    Added animations.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-$('.trigger-top').click(() => {
-    $('html, body').animate({ scrollTop: 0 }, 1000);
-})
+$(document).ready(() => {
 
-$('#fun-spinny-boi').click(() => {
-    $('#fun-spinny-boi').addClass('do-thing')
-})
+    $('.trigger-top').click(() => {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+    })
 
-$('#fun-spinny-boi').on('animationend', () => {
-    $('#fun-spinny-boi').removeClass('do-thing')
+    $('#fun-spinny-boi').click(() => {
+        $('#fun-spinny-boi').addClass('do-thing')
+    })
+
+    $('#fun-spinny-boi').on('animationend', () => {
+        $('#fun-spinny-boi').removeClass('do-thing')
+    })
+
+    $('.animated').on('animationend', (e) => {
+        if ($(e.target).hasClass('animated')) {
+            $(e.target).removeClass('animated fadeIn fadeInUp fadeInDown slow flip delay-0p5s delay-1s delay-1p5s delay-2s delay-2p5s delay-3s delay-3p5s delay-4s delay-4p5s delay-5s')
+        }
+    })
+
 })

17a694daaabab5ca37af001f497419d5c79321cd
Ben S <bencraft1392@gmail.com>
2019-11-04 16:50:03 -0500

    Added cool little effect.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $('.trigger-top').click(() => {
-    $('html, body').animate({ scrollTop: 0 });
+    $('html, body').animate({ scrollTop: 0 }, 1000);
+})
+
+$('#fun-spinny-boi').click(() => {
+    $('#fun-spinny-boi').addClass('do-thing')
+})
+
+$('#fun-spinny-boi').on('animationend', () => {
+    $('#fun-spinny-boi').removeClass('do-thing')
 })
